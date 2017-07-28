2c85becc3609b9bb6e2604597e2ef25bd0f3ba2e
Efe Gürkan YALAMAN <efe-gurkan.yalaman@trivago.com>
2017-07-28 16:01:27 +0200

    Adjust coverage options.
    We gather coverage from untested files as well now.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  testEnvironment: 'node'
+    testEnvironment: 'node',
+    collectCoverageFrom : [
+        '**/*.js',
+        '!**/node_modules/**',
+        '!*jest.config.js',
+        '!**/coverage/**'
+    ]
 }
